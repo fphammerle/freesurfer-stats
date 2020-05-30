c629b167d4e82ebbf398a383d5045251e42562cf
Fabian Peter Hammerle <fabian@hammerle.me>
2020-05-30 15:23:23 +0200

    wrap long lines to fix pylint warnings
    https://travis-ci.org/github/fphammerle/freesurfer-stats/jobs/692845491

--- tests/test_cortical_parcellation_stats.py
+++ tests/test_cortical_parcellation_stats.py
@@ -48,8 +48,10 @@ from freesurfer_stats import CorticalParcellationStats
                     2019, 5, 9, 21, 5, 54, tzinfo=datetime.timezone.utc
                 ),
                 "generating_program": "mris_anatomical_stats",
-                "cvs_version": "Id: mris_anatomical_stats.c,v 1.79 2016/03/14 15:15:34 greve Exp",
-                "mrisurf.c-cvs_version": "Id: mrisurf.c,v 1.781.2.6 2016/12/27 16:47:14 zkaufman Exp",
+                "cvs_version": "Id: mris_anatomical_stats.c,v 1.79"
+                " 2016/03/14 15:15:34 greve Exp",
+                "mrisurf.c-cvs_version": "Id: mrisurf.c,v 1.781.2.6"
+                " 2016/12/27 16:47:14 zkaufman Exp",
                 "cmdline": "mris_anatomical_stats -th3 -mgz -cortex ../label/lh.cortex.label"
                 " -f ../stats/lh.aparc.DKTatlas.stats -b -a ../label/lh.aparc.DKTatlas.annot"
                 " -c ../label/aparc.annot.DKTatlas.ctab fabian lh white",
@@ -123,8 +125,10 @@ from freesurfer_stats import CorticalParcellationStats
                     2019, 5, 9, 21, 3, 42, tzinfo=datetime.timezone.utc
                 ),
                 "generating_program": "mris_anatomical_stats",
-                "cvs_version": "Id: mris_anatomical_stats.c,v 1.79 2016/03/14 15:15:34 greve Exp",
-                "mrisurf.c-cvs_version": "Id: mrisurf.c,v 1.781.2.6 2016/12/27 16:47:14 zkaufman Exp",
+                "cvs_version": "Id: mris_anatomical_stats.c,v 1.79"
+                " 2016/03/14 15:15:34 greve Exp",
+                "mrisurf.c-cvs_version": "Id: mrisurf.c,v 1.781.2.6"
+                " 2016/12/27 16:47:14 zkaufman Exp",
                 "cmdline": "mris_anatomical_stats -th3 -mgz -cortex ../label/rh.cortex.label"
                 " -f ../stats/rh.aparc.pial.stats -b -a ../label/rh.aparc.annot"
                 " -c ../label/aparc.annot.ctab fabian rh pial",
